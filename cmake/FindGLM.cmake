d9a11a0aa886ca1ca14670b35478d97d709be509
Tatsuya Yatagawa <tatsy.mail@gmail.com>
2022-01-29 09:42:05 +0900

    Minor update.

--- cmake/FindGLM.cmake
+++ cmake/FindGLM.cmake
@@ -12,13 +12,13 @@ find_path(GLM_INCLUDE_DIR
           ${GLM_DIR}/include)
 
 find_package_handle_standard_args(
-    GLM
-    DEFAULT_MSG
-    GLM_INCLUDE_DIR
+  GLM
+  DEFAULT_MSG
+  GLM_INCLUDE_DIR
 )
 
 if (GLM_FOUND)
-    message(STATUS "GLM include: ${GLM_INCLUDE_DIR}")
-    set(GLM_INCLUDE_DIRS "${GLM_INCLUDE_DIR}" CACHE PATH "")
-    mark_as_advanced(GLM_INCLUDE_DIR)
+  message(STATUS "GLM include: ${GLM_INCLUDE_DIR}")
+  set(GLM_INCLUDE_DIRS "${GLM_INCLUDE_DIR}" CACHE PATH "")
+  mark_as_advanced(GLM_INCLUDE_DIR)
 endif()
